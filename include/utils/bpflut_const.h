f086c8afa47949cdaee5d7631c74630e5553c0f0
phuchainguyen <phuchai0610@gmail.com>
2025-07-17 05:40:25 +0700

    update bpflut path

--- include/utils/bpflut_const.h
+++ include/utils/bpflut_const.h
@@ -2,7 +2,7 @@
 #define BPFLUT_CONST_H
 
 #define BPFLUT_WORKSPACE_DIR "_bpflut_workspace_"
-#define BPFLUT_DIR BPFLUT_WORKSPACE_DIR "/bpflut"
+#define BPFLUT_DIR BPFLUT_WORKSPACE_DIR "/.bpflut"
 #define BPFLUT_IOS_DIR BPFLUT_DIR "/ios"
 #define BPFLUT_IOS_RUNNER_DIR BPFLUT_WORKSPACE_DIR "ios/Runner"
 #define BPFLUT_IOS_EXPORTOPTIONS_PATH BPFLUT_IOS_RUNNER_DIR "ExportOptions.plist"
